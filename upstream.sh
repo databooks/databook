b9fcaa9cc74d3e0c31639c39f77155b992f2a827
OpenThings <openthings@163.com>
2018-07-22 06:31:10 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -1,1 +1,11 @@
+echo ""
+echo "添加上游仓库，名称为upstream，然后可以使用upstream-pull.sh来同步到自己的库。"
+echo "Add upstream repository, then using upstream-pull.sh to sync into my repository"
+echo ""
+
 git add remote upstream https://github.com/openthings/databook
+
+echo ""
+echo "The git status:"
+git remote -v
+echo ""
