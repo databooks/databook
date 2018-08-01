18ad01ec28e4557f4f2919767258f9ba76484faf
openthings <openthings@163.com>
2018-08-01 20:32:02 +0800

    add sh tool push images to aliyun.

---
+++ build-tools/databook-images-pullfromaliyun.sh
@@ -0,0 +1,15 @@
+## 添加Tag for registry.cn-hangzhou.aliyuncs.com/openthings
+MY_REGISTRY=registry.cn-hangzhou.aliyuncs.com/openthings
+
+MY_IMAGE_DATABOOK=databook:latest
+
+## Pull镜像
+
+echo ""
+echo "1. pull $(MY_REGISTRY)/$(MY_IMAGE_DATABOOK)"
+
+docker pull ${MY_REGISTRY}/${MY_IMAGE_DATABOOK}
+docker tag ${MY_REGISTRY}/${MY_IMAGE_DATABOOK} openthings/${MY_IMAGE_DATABOOK}
+
+echo ""
+
