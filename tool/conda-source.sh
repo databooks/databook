3bd3b3dd04e81c13da0b5cb23c76759969d7016d
openthings <openthings@163.com>
2019-03-09 06:32:40 +0800

    update.

--- tool/conda-source.sh
+++ tool/conda-source.sh
@@ -2,3 +2,5 @@ conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/f
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
 conda config --set show_channel_urls yes
+
+mkdir ~/.pip/
