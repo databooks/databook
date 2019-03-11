8bacb930c26558320ed937959ab3880ca906cf30
openthings <openthings@163.com>
2019-03-11 15:34:24 +0800

    add deepml and pytorch tutorials.

--- build-tools/databook-images-pullfromaliyun.sh
+++ build-tools/databook-images-pullfromaliyun.sh
@@ -6,7 +6,7 @@ MY_IMAGE_DATABOOK=databook:latest
 ## Pull镜像
 
 echo ""
-echo "1. pull $(MY_REGISTRY)/$(MY_IMAGE_DATABOOK)"
+echo "1. pull ${MY_REGISTRY}/${MY_IMAGE_DATABOOK}"
 
 docker pull ${MY_REGISTRY}/${MY_IMAGE_DATABOOK}
 docker tag ${MY_REGISTRY}/${MY_IMAGE_DATABOOK} openthings/${MY_IMAGE_DATABOOK}
