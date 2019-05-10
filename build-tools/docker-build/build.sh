829256bb08d80b71bc9afcd52b3eee62f7398a07
openthings <openthings@163.com>
2019-05-10 15:33:36 +0800

    updat

--- build-tools/docker-build/build.sh
+++ build-tools/docker-build/build.sh
@@ -1,1 +1,22 @@
+echo ""
+echo "========================================================"
+echo "Prepare SuperMap iObjects Java packages ..."
+
+mkdir -p ./SuperMap/iobjectsjava911/Bin
+cp -r ../../../resources/Bin ./SuperMap/iobjectsjava911/
+cp -r ../../../resources/supermap_any_2019.lic9d ./SuperMap/supermap_any_2019.lic9d
+
+echo ""
+echo "========================================================"
+echo "Build docker openthings/databook ..."
 docker build --build-arg "HTTP_PROXY=http://192.168.199.99:9999" -t openthings/databook . 
+
+echo ""
+echo "========================================================"
+echo "Remove SuperMap iObjects Java packages ..."
+rm -r ./SuperMap
+
+echo ""
+echo "Build openthings/databook Finished, view: docker images."
+echo "========================================================"
+echo ""
