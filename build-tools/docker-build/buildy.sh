6c639cd6956da2ec84a17d79498249210af7ada3
openthings <openthings@163.com>
2019-05-13 11:43:38 +0800

    add cloud build tool.

---
+++ build-tools/docker-build/buildy.sh
@@ -0,0 +1,24 @@
+#docker build --build-arg "HTTP_PROXY=10.1.1.174:9999" --build-arg "HTTPS_PROXY=10.1.1.174:9999" -t openthings/databook .
+
+echo ""
+echo "========================================================"
+echo "Prepare SuperMap iObjects Java packages ..."
+
+mkdir -p ./SuperMap/iobjectsjava911/Bin
+cp -r ../../../resources/Bin ./SuperMap/iobjectsjava911/
+cp -r ../../../resources/supermap_any_2019.lic9d ./SuperMap/supermap_any_2019.lic9d
+
+echo ""
+echo "========================================================"
+echo "Build docker openthings/databook ..."
+docker build -f Dockerfiley -t openthings/databook .
+
+echo ""
+echo "========================================================"
+echo "Remove SuperMap iObjects Java packages ..."
+rm -r ./SuperMap
+
+echo ""
+echo "Build openthings/databook Finished, view: docker images."
+echo "========================================================"
+echo ""
