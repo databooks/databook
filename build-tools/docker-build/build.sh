7956a4781fe92e1550aed4185f9487195c24dc22
openthings <openthings@163.com>
2019-05-17 06:23:15 +0800

    update build.sh

--- build-tools/docker-build/build.sh
+++ build-tools/docker-build/build.sh
@@ -1,1 +1,1 @@
-docker build --build-arg "HTTP_PROXY=http://192.168.199.99:9999" -t openthings/databook . 
+docker build --build-arg "HTTP_PROXY=http://192.168.199.180:9999" -t openthings/databook . 
